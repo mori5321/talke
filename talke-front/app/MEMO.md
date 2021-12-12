14cc5ad64b6627bc17b52721df47e425ae334d99
mori5321 <moriwm77@gmail.com>
2021-12-13 00:53:58 +0900

    chore: setup typescript in next app

---
+++ talke-front/app/MEMO.md
@@ -0,0 +1,2 @@
+Allow importing CSS modules from other npm package.
+https://github.com/vercel/next.js/issues/13282
---
+++ talke-front/app/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+// import Head from "next/head";
+// import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const IndexPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello World</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
