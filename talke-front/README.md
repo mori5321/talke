75fb34e6abfd39a6ae733c18d56cb69631fc391b
mori5321 <moriwm77@gmail.com>
2021-12-29 18:22:04 +0900

    fix: update storybook to fix launching bug

---
+++ talke-front/README.md
@@ -0,0 +1,6 @@
+# Command
+## Run local
+npm run dev --workspace app
+
+## Run storybook
+npm run storybook --workspace libs/components
--- talke-front/libs/components/src/ui/Buttons/index.tsx
+++
@@ -1,5 +0,0 @@
-type ButtonProps = {
-  text: string;
-};
-
-const Button = ({ text }: ButtonProps): JSX.Element => <button>{text}</button>;
